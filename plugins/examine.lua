a00c52512815dedea346c05076063423e720c103
Jordan Jay <68760809+iilegacyyii@users.noreply.github.com>
2025-08-28 17:47:54 +0100

    Add format and option to examine command
    Enhanced the examine plugin to support format specifiers (word, dword, qword) via command-line arguments. Refactored memory reading functions to return both data and size, and added new methods for reading word, dword, and qword values from memory.
    
    The sizes are mostly useless for the time being, but will be useful later when dealing with more complex data types or strings

--- plugins/examine.lua
+++ plugins/examine.lua
@@ -10,7 +10,7 @@ function examine:parseargs(args)
     parser:init("examine", "display memory contents at a given address", false)
     parser:AddArgument("address", "address of memory content", true, "store", Evaluate)
     -- TODO: implement format specifiers
-    --parser:AddArgument({"-f", "--format"}, "format to display data (e.g. gx)", false, "store", nil)
+    parser:AddArgument({"-f", "--format"}, "format to display data (e.g. gx)", false, "store", nil)
     parser:AddArgument({"-c", "--count"}, "number of items to display (e.g. 10)", false, "store", math.tointeger)
     return parser:ParseArgs(args)
 end
@@ -19,24 +19,75 @@ function examine:command(args)
     local namespace = examine:parseargs(args)
     if namespace == nil then return end
 
+    examine.valid_formats["word"] = examine.word;
+    examine.valid_formats["dword"] = examine.dword;
+    examine.valid_formats["qword"] = examine.qword;
+
     ---@type integer
     local address = namespace["address"]
+    if address == nil then
+        print("usage: examine <address> [--format format] [--count count]")
+        return
+    end
 
     ---@type integer
     local count = 1
     if namespace["--count"] ~= nil then count = namespace["--count"] end
 
-    local ptr_size = 4
-    if Is64BitTarget() then ptr_size = 8 end
+    -- Set format, defaults to pointer size, otherwise user format
+    ---@type string
+    local format
+    if Is64BitTarget() then format = "qword" else format = "dword" end
+    for valid_format, func in pairs(examine.valid_formats) do
+        if namespace["--format"] == valid_format then
+            format = namespace["--format"]
+            goto format_processed
+        end
+    end
+    ::format_processed::
+
     local to_print = ""
+    local base_addr = address
+    local last_size = 0
     for i = 0, count-1 do
-        local data = memory:readptr(address + (i*ptr_size))
+        local data
+        data, last_size = examine.valid_formats[format](self, address)
         if data == nil then
-            printf("Failed to read data at address %s", address2hex(address + (i*ptr_size)))
+            printf("Failed to read data at address %s", address2hex(address))
             goto output
         end
-        to_print = to_print .. string.format("%02x:%04x| " .. colour.LIGHTBLUE .. "%s" .. colour.DEFAULT .. " : %s", i, i*ptr_size, address2hex(address + (i*ptr_size)), address2hex(data)) .. string.char(10)
+        to_print = to_print .. string.format("%02x:%04x| %s%s%s : ", i, address - base_addr, colour.LIGHTBLUE, address2hex(address), colour.DEFAULT)
+        to_print = to_print .. data .. string.char(10)
+
+        address = address + last_size
     end
     ::output::
     print(to_print)
+end
+
+---Read a qword at address, returns string representation of data & size
+---@param address integer
+---@return string|nil, integer
+function examine:qword(address)
+    local data, size = memory:readqword(address)
+    if data == nil then return nil, size end
+    return string.format("0x%016x", data), size
+end
+
+---Read a dword at address, returns string representation of data & size
+---@param address integer
+---@return string|nil, integer
+function examine:dword(address)
+    local data, size = memory:readdword(address)
+    if data == nil then return nil, size end
+    return string.format("0x%08x", data), size
+end
+
+---Read a word at address, returns string representation of data & size
+---@param address integer
+---@return string|nil, integer
+function examine:word(address)
+    local data, size = memory:readword(address)
+    if data == nil then return nil, size end
+    return string.format("0x%02x", data), size
 end
--- plugins/memory.lua
+++ plugins/memory.lua
@@ -44,38 +44,61 @@ function memory:prot2str(prot)
     return out
 end
 
----Read from target memory. Returns nil on failure
+---Read from target memory.
+---Returns data, datasize (data == nil on failure)
 ---@param address integer
 ---@param size integer
----@return string|nil
+---@return string|nil, integer
 function memory:read(address, size)
     local success
     local data
     success, data = pcall(function(a, s) return ReadMemory(a, s) end, address, size)
-    if success == false then return nil end
-    return data
+    if success == false then return nil, size end
+    return data, size
 end
 
----Read ptr_size integer from target memory. Returns nil on failure
+---Read ptr_size integer from target memory.
+---Returns data, datasize (data == nil on failure)
 ---@param address integer
----@return integer|nil
+---@return integer|nil, integer
 function memory:readptr(address)
     local ptr_size = 4
     if Is64BitTarget() then ptr_size = 8 end
     local data = memory:read(address, ptr_size)
-    if data == nil then return nil end
+    if data == nil then return nil, ptr_size end
 
     if ptr_size == 4 then
-        return string.unpack("<i4", data)
+        return string.unpack("<i4", data), ptr_size
     end
-    return string.unpack("<i8", data)
+    return string.unpack("<i8", data), ptr_size
 end
 
----Read qword from target memory. Returns nil on failure
+---Read dword from target memory.
+---Returns data, datasize (data == nil on failure)
 ---@param address integer
----@return integer|nil
+---@return integer|nil, integer
+function memory:readword(address)
+    local data = memory:read(address, 2)
+    if data == nil then return nil, 2 end
+    return string.unpack("<i2", data) & 0xFFFF, 2
+end
+
+---Read dword from target memory.
+---Returns data, datasize (data == nil on failure)
+---@param address integer
+---@return integer|nil, integer
+function memory:readdword(address)
+    local data = memory:read(address, 4)
+    if data == nil then return nil, 4 end
+    return string.unpack("<i4", data) & 0xFFFFFFFF, 4
+end
+
+---Read qword from target memory.
+---Returns data, datasize (data == nil on failure)
+---@param address integer
+---@return integer|nil, integer
 function memory:readqword(address)
     local data = memory:read(address, 8)
-    if data == nil then return nil end
-    return string.unpack("<i8", data)
+    if data == nil then return nil, 8 end
+    return string.unpack("<i8", data), 8
 end
